ab08568c5933a84361a505f30be98460ff0d3225
jubao <tqy_4896@hotmail.com>
2020-06-23 23:24:19 +0800

    Initial Commit

---
+++ 557021-hw1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  557021-hw1
+//
+//  Created by User16 on 2020/6/23.
+//  Copyright © 2020 ntou. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
